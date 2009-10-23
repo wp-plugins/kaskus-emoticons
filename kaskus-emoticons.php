b165342ed878d616a437ce756b0a0ffcc6582ddb
nartzco <nartzco@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-23 03:40:29 +0000

    tagging version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/kaskus-emoticons/tags/1.0@166086 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ kaskus-emoticons.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Plugin Name: Kaskus Emoticons
+Plugin URI: http://nartzco.com/blog/2009/10/23/kaskus-emoticons/
+Description: Kaskus Emoticons are often use in the Kaskus forum. <a href="http://kaskus.us">Kaskus</a> is The Largest <strong>Indonesian</strong> Community
+Version: 1.0
+Author: Rehybrid
+Author URI: http://nartzco.com/
+
+Copyright 2009, Rehybrid
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+add_filter('the_content',array('KaskusEmoticons','replace'));
+add_filter('comment_text',array('KaskusEmoticons','replace'));
+
+require_once("kaskus-emoticons-list.php");
+
+if(!class_exists('KaskusEmoticons')){
+	class KaskusEmoticons {
+		function replace($content){
+			global $KEReplace;
+			//print_r($content);die;
+			return strtr($content,$KEReplace);
+		}
+	}
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Kaskus Emoticons ===
+Contributors: rehybrid
+Donate link:
+Tags: kaskus,emoticons,comments,posts,smileys,smilies 
+Requires at least: 2.7.1
+Tested up to: 2.8.5
+
+
+Kaskus Emoticons are often use in the Kaskus forum. Kaskus is The Largest Indonesian Community
+
+== Description ==
+
+Kaskus Emoticons are often use in the Kaskus forum. Kaskus is The Largest Indonesian Community
+
+== Installation ==
+
+1. Upload `kaskus-emoticons` to your `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Screenshots ==
+
+1. List Emoticon
+
+== Changelog ==
+
+= 1.0 =
+ * New plugin published.
