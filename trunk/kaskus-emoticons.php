273c63d20bb2a0490a6403bcd1de484415218813
nartzco <nartzco@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-25 03:44:26 +0000

    tagging version 2.1
    git-svn-id: https://plugins.svn.wordpress.org/kaskus-emoticons/tags/2.0@176833 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/kaskus-emoticons.php
@@ -0,0 +1,144 @@
+<?php
+/*
+Plugin Name: Kaskus Emoticons
+Plugin URI: http://nartzco.com/blog/2009/10/23/kaskus-emoticons/
+Description: Kaskus Emoticons is an emoticon set inspired by Kaskus, the Largest Indonesian Community - consisting of over a million active members from all over the world. The images which are used in this plugin are copyright of Kaskus
+Version: 2.1
+Author: Rehybrid
+Author URI: http://nartzco.com/
+
+Copyright 2009, nartzco.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+register_activation_hook( __FILE__, array('KaskusEmoticons', 'activate'));
+register_deactivation_hook( __FILE__, array('KaskusEmoticons', 'deactivate'));
+add_filter('the_content',array('KaskusEmoticons','replace'));
+add_filter('comment_text',array('KaskusEmoticons','replace'));
+add_action('comment_form', array('KaskusEmoticons', 'scut'));
+add_action('wp_head', array('KaskusEmoticons', 'script'));
+add_action('admin_menu', array('KaskusEmoticons','menu'));
+add_filter( 'plugin_action_links', array('KaskusEmoticons', 'link'), 10, 2 );
+require_once("kaskus-emoticons-list.php");
+
+if(!class_exists('KaskusEmoticons')){
+	class KaskusEmoticons {
+		function activate(){
+			$data = array(
+				'backlink'=> 1
+			);
+	    	if (!get_option('kaskus_emoticons')){
+	      		add_option('kaskus_emoticons', $data);
+	    	} else {
+	      		update_option('kaskus_emoticons', $data);
+	    	}		
+		}
+		
+		function deactivate(){
+			delete_option('kaskus_emoticons');
+		}
+
+		function link( $links, $file ){
+			static $this_plugin;
+			if ( ! $this_plugin ) $this_plugin = plugin_basename(__FILE__);
+			
+			if ( $file == $this_plugin ){
+				$settings_link = '<a href="options-general.php?page=KaskusEmoticons">' . __('Settings') . '</a>';
+				array_unshift( $links, $settings_link ); // before other links
+			}
+			return $links;
+		}
+		
+		function menu(){
+			add_options_page('Kaskus Emoticons', 'Kaskus Emoticons', 8, 'KaskusEmoticons', array('KaskusEmoticons','control'));
+		}
+
+		function control() {
+			$options = $newoptions = get_option('kaskus_emoticons');
+			if($_POST["kaskus_emoticons_action"]) {
+				//print_r($_POST);
+				$newoptions['backlink'] 	= strip_tags(stripslashes($_POST["kaskus_emoticons_backlink"]));
+				if(trim($newoptions['backlink'])=="") $newoptions['backlink'] = 1;
+			}
+			if ($options != $newoptions) {
+				$options = $newoptions;
+				//print_r($options);
+				update_option('kaskus_emoticons', $options);
+			}
+			
+			$backlink= htmlspecialchars($options['backlink'], ENT_QUOTES);
+?>
+			<h2>KASKUS EMOTICONS</h2>
+			<em>WordPress plugin written by <a href="http://nartzco.com">Rehybrid</a> </em><br /><br />
+			<form method="post" action="options-general.php?page=KaskusEmoticons">
+			<table>
+			<tr>
+			<td><?php _e('FOR BACKLINK ! You can disable this, but if you enable it, Thanks!'); ?></td>
+			<td>
+				<select name="kaskus_emoticons_backlink">
+					<option value="1"<?php echo($backlink=="1"?" selected":"")?>>Enable</option>
+					<option value="0"<?php echo($backlink=="1"?"":" selected")?>>Disable</option>
+				</select>
+			</td>
+			</tr>
+			</table>
+			<input type="hidden" id="kaskus_emoticons_action" name="kaskus_emoticons_action" value="1" /><br />
+			<input type="submit" id="kaskus_emoticons_submit" name="kaskus_emoticons_submit" value="Save Settings" />
+			</form>
+<?php
+		}
+
+		function replace($content){
+			global $KEReplace;
+			//print_r($content);die;
+			return strtr($content,$KEReplace);
+		}
+		
+		function scut(){
+			global $KEReplace;
+			$opt = get_option('kaskus_emoticons');
+			echo "<div id='kaskusemoticonslink' style=\"cursor:pointer\" onclick=\"kaskusemoticonsclink()\">[+] kaskus emoticons</div>";
+			echo "<div id='kaskusemoticonscontent' style=\"display:none\">";
+			foreach($KEReplace as $k=>$v){
+				echo "<a title=\"".$k."\" href=\"javascript:kaskusemoticonsclick('".$k."')\" style=\"cursor:pointer;margin:1px;border:none\">".$v."</a>";
+			}
+			if(isset($opt['backlink']) && $opt['backlink']) echo "<br>Kaskus Emoticons by <a rel=\"follow\" href=\"http://nartzco.com\">nartzco.com</a>";
+			else {
+				if(!isset($opt['backlink'])) echo "<br>Kaskus Emoticons by <a rel=\"follow\" href=\"http://nartzco.com\">nartzco.com</a>";
+			}
+			echo "</div>"; 
+		}
+		
+		function script(){
+?>
+<script language="javascript">
+	var gOI = function(id){
+		return document.getElementById(id);
+	};
+	
+	var kaskusemoticonsclick = function(tag){
+		gOI("comment").value = gOI("comment").value + " " + tag;
+	};
+	
+	var kaskusemoticonsclink = function(){
+		gOI("kaskusemoticonslink").innerHTML = gOI("kaskusemoticonscontent").style.display == "" ? "[+] kaskus emoticons":"[-] kaskus emoticons";
+		gOI("kaskusemoticonscontent").style.display = gOI("kaskusemoticonscontent").style.display == "" ? "none":"";
+	};
+</script>	
+<?php
+		}
+	}
+}
+?>
---
+++ trunk/readme.txt
@@ -0,0 +1,30 @@
+=== Kaskus Emoticons ===
+Contributors: rehybrid
+Donate link:
+Tags: kaskus,emoticons,comments,posts,smileys,smilies 
+Requires at least: 2.7.1
+Tested up to: 2.8.5
+Stable tag: 2.1
+
+Kaskus Emoticons is an emoticon set inspired by Kaskus
+== Description ==
+Kaskus Emoticons is an emoticon set inspired by Kaskus, the Largest Indonesian Community - consisting of over a million active members from all over the world. The images which are used in this plugin are copyright of Kaskus
+== Installation ==
+
+1. Upload `kaskus-emoticons` to your `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Screenshots ==
+
+1. List Emoticon
+
+== Changelog ==
+= 2.1 =
+ * added two emoticon
+= 2.0 =
+ * added shortcut in the comment form
+ * added two emoticon
+= 1.0.1 =
+ * change description.
+= 1.0 =
+ * New plugin published.
